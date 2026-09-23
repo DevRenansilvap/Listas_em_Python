75fd261b10a5073a34f5c0e6c2ff14a5d8323b52
DevRenans <renansilvapa@gmail.com>
2026-09-22 23:04:30 -0300

    Adicionando primeiro commit

---
+++ Listas.py
@@ -0,0 +1,34 @@
+# Praticando Listas Em Python
+
+l1 = ['Renan','Yara','Bruno']
+print(l1)
+
+#append adiciona um elemento na lista
+
+# l1.append("João")
+# print(l1)
+
+# #extend adiciona mais de 1 elemento na lista ou adiciona os elementos de uma lista na lista
+
+# l2 = l1
+# l1.extend(l2)
+
+# print(l1)
+# lista4=[1,2,3]
+# l2.extend(lista4)
+# print(l2)
+# #copy ele compia a lista
+
+# l3 = l1.copy()
+# print(l1)
+# print(l3)
+
+# #count conta quantas vezes o elemento esta dentro da lista
+# l1.append('Renan')
+# print(l1.count('Renan'))
+# print(l1.count('Yara'))
+
+#index diz a posição do item na lista
+
+print(l1.index('Yara'))
+print(l1.index('Renan'))
